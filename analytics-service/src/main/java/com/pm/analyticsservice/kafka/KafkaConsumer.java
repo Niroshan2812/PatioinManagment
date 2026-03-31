f75d2eb9879811917b15fd2ef2df7e77640f46e7
Niroshan <niroshan9812@gmail.com>
2026-04-01 01:41:26 +0530

    feat: Init  Kafka consumer and Protobuf schema

--- analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -1,4 +1,34 @@
 package com.pm.analyticsservice.kafka;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
 public class KafkaConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics="patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event){
+        //convert byte array
+        try {
+            // create pation object from pation bytearray
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            // perform any business related to analytics here
+            log.info("Received Patient Event: [patientID={}, " +
+                    "PatientName={}, PatientEmail={} ] ",
+                    patientEvent.getPatientID(),
+                    patientEvent.getName(),
+                    patientEvent.getEmail());
+
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing event {} ",e.getMessage());
+        }
+
+
+    }
 }
